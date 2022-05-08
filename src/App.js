b93fde8e0dc4889d78086bafcb30acc9b91834c9
zoubinnaba <emmanuelzoubinnaba@gmail.com>
2022-05-08 13:53:52 +0000

    Create app with darkMode toggle

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+import Navbar from "./components/Navbar"
+import Main from "./components/Main"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+
+    const [darkMode, setDarkeMode] = useState(true)
+
+    function toggleDarkMode() {
+        setDarkeMode(prevTheme => !prevTheme)
+    }
+
+    return (
+        <div className="container">
+            <Navbar 
+                darkMode={darkMode}
+                toggleDarkMode={toggleDarkMode}
+            />
+            <Main darkMode={darkMode}/>
+        </div>
+    )
 }
-
-export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,30 @@
+import logo from "../assets/logo.svg"
+
+export default function Navbar(props) {
+
+    return (
+         <nav 
+            className={props.darkMode ? "dark": ""}
+        >
+            <img 
+                className="nav--logo_icon"
+                src={logo}
+                alt="logo"
+            />
+            <h3 className="nav--logo_text">ReactFacts</h3>
+            
+            <div 
+                className="toggler" 
+            >
+                <p className="toggler--light">Light</p>
+                <div 
+                    className="toggler--slider"
+                    onClick={props.toggleDarkMode}
+                >
+                    <div className="toggler--slider--circle"></div>
+                </div>
+                <p className="toggler--dark">Dark</p>
+            </div>
+        </nav>
+    )
+}
